903d7e142ac941aefdf27eced0d5fc7bfde335ed
Mwayi Kashoko <39887498+MwayiKashoko@users.noreply.github.com>
2025-06-17 12:08:35 -0500

    Update Solitaire.js

--- Solitaire.js
+++ Solitaire.js
@@ -96,6 +96,15 @@ const graphics = canvas.getContext("2d");
 
 	let fromPlaced = false;
 
+	const getMousePos = (evt) => {
+		const rect = canvas.getBoundingClientRect();
+
+		return {
+			x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+			y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+		};
+	}
+
 	canvas.addEventListener("mousedown", function(mouse) {
 		mouseDown = true;
 		let index = 0;
